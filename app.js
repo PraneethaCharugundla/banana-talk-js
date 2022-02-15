d49f286ad827a2c7538152cc291f21c0e879dd35
praneethashetty <praneethach2000@gmail.com>
2022-02-15 15:53:16 +0530

    error handling added

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 var btntranslate = document.querySelector("#btn-translate")
 
 var txtInput =document.querySelector("#txt-input");
-var outputDiv= document.querySelector("#output")
+var outputDiv= document.querySelector("#output");
 
-console.log(outputDiv);
+
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("something wrong with server ! try after sometime")
+}
+
+   
 function clickHandler() {
-  outputDiv.innerText = "asdhgd" + txtInput.value 
+ var inputText = txtInput.value; //taking input
+
+ fetch(getTranslationURL(inputText))
+   .then(reponse => reponse.json())
+   .then(json => {
+var tarnslatedText = json.contents.translated;
+    outputDiv.innerText = tarnslatedText; //output
+   })
+    .catch(errorHandler)
+
 };
 
 btntranslate.addEventListener("click",clickHandler)
